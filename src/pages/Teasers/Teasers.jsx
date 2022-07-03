6cd23eb0a07a26f8fc674b3f16a3f671d7833475
Aman Rawat <amanrawat4060@gmail.com>
2022-07-03 16:09:55 +0530

    added Teasers.jsx

--- src/pages/Teasers/Teasers.jsx
+++ src/pages/Teasers/Teasers.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import CircularHeader from "../../components/CircularHeader/CircularHeader";
 import SeaDawgMark from "../../components/SeaDawgMark/SeaDawgMark";
-import "./Teasers.scss"
+import "./Teasers.scss";
 
 function Teasers() {
     return (
@@ -10,19 +10,15 @@ function Teasers() {
             <SeaDawgMark />
             <CircularHeader heading="TEASERS">
                 <div className="content_first">
-                    As you would learn from our story (Who We Are), we were
-                    brought into being in order to fill a void. We saw the need
-                    for something to be done, observed that nobody was really
-                    doing it, and took it upon ourselves to take up the task.
+                    We will release teasers under this section for upcoming
+                    products and events.
                     <br />
                     <br />
-                    But this isn’t the only thing we want to do. We don’t just
-                    want to design material things to sell them and make profit.
-                    This entity was established not by seeing a means to profit
-                    from the observed void, but to explore it and do our best to
-                    get rid of it.
+                    WATCH THIS SPACE!
                     <br />
                     <br />
+                    In the meantime, you may consider following us on social
+                    media as well. We are even more regular out there.
                 </div>
             </CircularHeader>
         </div>
