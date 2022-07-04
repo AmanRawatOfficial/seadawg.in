86d4ec89a679ab580a02d327da2a9a0e16125396
Aman Rawat <amanrawat4060@gmail.com>
2022-07-04 09:43:22 +0530

    Added Comic stripe Page

--- src/pages/Intro/Intro.jsx
+++ src/pages/Intro/Intro.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 
-
+import SeaDawgMark from '../../components/SeaDawgMark/SeaDawgMark'
 import "./Intro.scss"
 
 function Intro() {
     return (
         <div className='intro'>
-            
+            <SeaDawgMark />
+            <div className='comic_container'>
+                <div className='comic_stripe_container'>
+                    <div className='comic_stripe_element'></div>
+                    <div className='comic_stripe_element'></div>
+                    <div className='comic_stripe_element'></div>
+                </div>
+            </div>
         </div>
     )
 }
